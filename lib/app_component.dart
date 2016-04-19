f1face42f86c1e2a8b5d8f7f44398821069248d9
Thanasis Voivodas <thanasis.voivodas@taxydromiki.gr>
2016-04-19 16:34:18 +0300

    adds the mdlupgrade directive.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,7 +1,11 @@
 // Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
+library app;
 
+import 'dart:js';
 import 'package:angular2/core.dart';
+
+part 'mdl_upgrade.dart';
 
 @Component(selector: 'my-app', templateUrl: 'app_component.html')
 class AppComponent {}
---
+++ lib/mdl_upgrade.dart
@@ -0,0 +1,18 @@
+part of app;
+
+
+@Directive(
+  selector: '[mdlUpgrade]'
+)
+
+class MdlUpgradeDirective implements OnInit{
+  ElementRef _elRef;
+  MdlUpgradeDirective(this._elRef);
+
+  @override
+  ngOnInit() {
+    JsObject componentHandler = context["componentHandler"];
+    componentHandler.callMethod("upgradeElement",
+          [new JsObject.fromBrowserObject(_elRef.nativeElement)]);
+  }
+}
